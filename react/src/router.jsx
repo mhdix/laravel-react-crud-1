cf20bad387228b6f0a2dfbd449de0b2e91018ad7
mhdix <mhdix1254@gmail.com>
2023-01-31 21:59:05 +0330

    router logic and create default and guest layoutes

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -3,29 +3,45 @@ import Login from "./views/Login.jsx";
 import SignUp from "./views/SignUp";
 import User from "./views/User";
 import NotFound from "./views/NotFound";
+import GuestLayout from "./components/GuestLayout";
+import DefaultLayout from "./components/DefaultLayout";
+import Dashboard from "./views/Dashboard";
 
 
 const router = createBrowserRouter([
     {
-        path: '/login',
-        element: <Login/>,
+        path: '/',
+        element: <GuestLayout/>,
+        children: [
+            {
+                path: '/login',
+                element: <Login/>,
+            },
+            {
+                path: '/signup',
+                element: <SignUp/>,
+            },
+        ]
     },
-
     {
-        path: '/signup',
-        element: <SignUp/>,
-    },
-
-    {
-        path: '/user',
-        element: <User/>
+        path: '/',
+        element: <DefaultLayout/>,
+        children: [
+            {
+                path: '/user',
+                element: <User/>
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard/>
+            },
+        ]
     },
 
     {
         path: '*',
         element: <NotFound/>
     },
-
-])
+]);
 
 export default router;
