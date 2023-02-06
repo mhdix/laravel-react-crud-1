e3fb61c9aeef0fa70fe704427e311102cceacc79
mhdix <mhdix1254@gmail.com>
2023-02-06 13:15:40 +0330

    program signup ui

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -8,6 +8,9 @@ export default function Login() {
         <div className='login-signup-form animated fadeInDown'>
             <div className="form">
                 <form onSubmit={onsubmit}>
+                    <h1 className='title'>
+                        Login into your account
+                    </h1>
                     <input type="email" placeholder='Email'/>
                     <input type="password" placeholder='Password'/>
                     <button className='btn btn-block'>Login</button>
--- react/src/views/SignUp.jsx
+++ react/src/views/SignUp.jsx
@@ -1,7 +1,26 @@
-export default function SignUp(){
-    return(
-    <>
-    Sign Up
-    </>
+import {Link} from "react-router-dom";
+
+export default function SignUp() {
+    const onsubmit = (e)=> {
+        e.preventDefault()
+    }
+    return (
+        <div className='login-signup-form animated fadeInDown'>
+            <div className="form">
+                <form onSubmit={onsubmit}>
+                    <h1 className='title'>
+                        Signup for free
+                    </h1>
+                    <input type="name" placeholder='Full Name'/>
+                    <input type="email" placeholder='Email'/>
+                    <input type="password" placeholder='Password'/>
+                    <input type="password" placeholder='Password Confirmation'/>
+                    <button className='btn btn-block'>Login</button>
+                    <p className='message'>
+                        Already Registered? <Link to="/login">Sign in</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
     )
 }
