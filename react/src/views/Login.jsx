b2a35e591c4263352e2a30101beea3f3b00e0a14
mhdix <mhdix1254@gmail.com>
2023-02-06 13:04:59 +0330

    program login ui

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,7 +1,21 @@
+import {Link} from "react-router-dom";
+
 export default function Login() {
+    const onsubmit = (e)=> {
+        e.preventDefault()
+    }
     return (
-        <div>
-            Login
+        <div className='login-signup-form animated fadeInDown'>
+            <div className="form">
+                <form onSubmit={onsubmit}>
+                    <input type="email" placeholder='Email'/>
+                    <input type="password" placeholder='Password'/>
+                    <button className='btn btn-block'>Login</button>
+                    <p className='message'>
+                        Not Registered ? <Link rel="stylesheet" href="/signup">Create an account</Link>
+                    </p>
+                </form>
+            </div>
         </div>
     )
 }
