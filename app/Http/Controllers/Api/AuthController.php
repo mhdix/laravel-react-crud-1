2badd13ff04593885e599d3a3cb4325e1ccb7374
mhdix <domanloom@gmail.com>
2023-02-07 13:00:52 +0330

    develop login signup & logout controller in AuthController

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function signup(SignupRequest $request)
+    {
+        /** @var User $user */
+        $user = $request->validated();
+        $user->create([
+            'name' => $user['name'],
+            'email' => $user['email'],
+            'password' => bcrypt($user['password']),
+        ]);
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user' , 'token'));
+    }
+
+    public function login(LoginRequest $request)
+    {
+
+        $credentials = $request->validated();
+        if (!Auth::attempt($credentials)){
+            response([
+                'message' => 'ایمیل یا پسوورد اشتباه است',
+            ]);
+        }
+        /** @var User $user */
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user', 'token'));
+
+    }
+
+    public function logout(Request $request)
+    {
+        /** @var User $user */
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+        return response('' , 204);
+    }
+}
