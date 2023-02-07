841f6454d958b7cf9da2144cc216dadeb57a5a81
mhdix <domanloom@gmail.com>
2023-02-07 16:42:17 +0330

    add axiosClient in login componnets

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,8 +1,22 @@
 import {Link} from "react-router-dom";
+import {useRef} from "react";
+import axiosClient from "../axios-client.js";
+import {useStateContext} from "../context/ContextProvider.jsx";
 
 export default function Login() {
-    const onsubmit = (e)=> {
+    const {setToken} = useStateContext()
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const onsubmit = (e) => {
         e.preventDefault()
+        const payload = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        }
+        axiosClient.post('/login', payload)
+            .then(({data}) => {
+                setToken(data.token)
+            })
     }
     return (
         <div className='login-signup-form animated fadeInDown'>
@@ -11,8 +25,8 @@ export default function Login() {
                     <h1 className='title'>
                         Login into your account
                     </h1>
-                    <input type="email" placeholder='Email'/>
-                    <input type="password" placeholder='Password'/>
+                    <input ref={emailRef} type="email" placeholder='Email'/>
+                    <input ref={passwordRef} type="password" placeholder='Password'/>
                     <button className='btn btn-block'>Login</button>
                     <p className='message'>
                         Not Registered ? <Link to="/signup">Create an account</Link>
