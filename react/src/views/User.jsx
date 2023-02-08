82077b06b22a81e815a6f6c5327da4f539434855
mhdix <domanloom@gmail.com>
2023-02-08 15:28:09 +0330

    fetch data in user component

--- react/src/views/User.jsx
+++ react/src/views/User.jsx
@@ -1,7 +1,31 @@
-export default function User(){
-    return(
-    <>
-    User
-    </>
+import {useEffect, useState} from "react";
+import axiosClient from "../axios-client.js";
+
+export default function User() {
+    const [users, setUsers] = useState()
+    const [loading, setLoading] = useState(false)
+
+
+    useEffect(() => {
+        getUser()
+    }, [])
+
+    const getUser = () => {
+        setLoading(true)
+        axiosClient.get('/user')
+            .then(({data}) => {
+                setUsers(data);
+                setLoading(false)
+            })
+            .catch(() => {
+                setLoading(false)
+            })
+    }
+
+    return (
+        <div>
+            Users
+            {<p>{!loading ? users?.name : <p>loading ...</p>}</p>}
+        </div>
     )
 }
