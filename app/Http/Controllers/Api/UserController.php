2afffdee7b1e92034a5c09add38251b541d065f8
mhdix <domanloom@gmail.com>
2023-02-08 15:59:03 +0330

    add userController crud logic

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
@@ -12,11 +13,13 @@ class UserController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function index()
     {
-        //
+        return UserResource::collection(
+            User::query()->orderBy('id' , 'desc')->paginate(10)
+        );
     }
 
     /**
@@ -27,18 +30,21 @@ class UserController extends Controller
      */
     public function store(StoreUserRequest $request)
     {
-        //
+        $data = $request->validated();
+        $data['password'] = bcrypt($data['password']);
+        $user = User::create($data);
+        return response(new UserResource($user) , 201);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
+     * @return UserResource
      */
     public function show(User $user)
     {
-        //
+        return new UserResource($user);
     }
 
     /**
@@ -46,11 +52,16 @@ class UserController extends Controller
      *
      * @param  \App\Http\Requests\UpdateUserRequest  $request
      * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
+     * @return UserResource
      */
     public function update(UpdateUserRequest $request, User $user)
     {
-        //
+        $data = $request->validated();
+        if (isset($data['password'])){
+            $data['password'] = bcrypt($data['password']);
+        }
+        $user->update($data);
+        return new UserResource($user);
     }
 
     /**
@@ -61,6 +72,7 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+         $user->delete();
+         return response('' ,204 );
     }
 }
