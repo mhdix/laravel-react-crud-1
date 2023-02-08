b5c9db809fdd7ea9ee1ab0c1f4c830e9f715e895
mhdix <domanloom@gmail.com>
2023-02-08 13:59:01 +0330

    create logout handler and login in ui

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,45 +1,48 @@
-import {Link, Navigate, Outlet} from "react-router-dom";
+import {Link, Navigate, Outlet, useNavigate} from "react-router-dom";
 import {useStateContext} from "../context/ContextProvider.jsx";
 import {useEffect, useState} from "react";
 import axiosClient from "../axios-client.js";
 
-export default function DefaultLayout(){
-    const {token , user , setUser} = useStateContext();
+export default function DefaultLayout() {
+    const {token, user, setUser, setToken} = useStateContext();
 
-
-
-    if (!token){
+    if (!token) {
         return <Navigate to='/login'/>
     }
-    useEffect(()=>{
-        axiosClient.get('/user').then(({data})=>{
-            setUser(data)
-        })
-    },[])
     const onLogout = (e) => {
         e.preventDefault();
+        axiosClient.post('/logout').then(()=>{
+            setToken(null)
+            setUser({})
+        })
     }
-    return(
-    <div id='defaultLayout'>
-        <aside>
-            <Link to='/dashboard' >Dashboard</Link>
-            <Link to='/user' >User</Link>
-        </aside>
-        <div className='content'>
-            <header>
-                <div>
-                    Header
-                </div>
-                <div>
-                    {user?.name}
-                    <a href="#" onClick={onLogout} className='btn-logout'>Logout</a>
-                </div>
-            </header>
-            <main>
-                <Outlet/>
-            </main>
-        </div>
+    useEffect(() => {
+        axiosClient.get('/user').then(({data}) => {
+            setUser(data)
+        })
+    }, [])
 
-    </div>
+    return (
+        <div id='defaultLayout'>
+            <aside>
+                <Link to='/dashboard'>Dashboard</Link>
+                <Link to='/user'>User</Link>
+            </aside>
+            <div className='content'>
+                <header>
+                    <div>
+                        Header
+                    </div>
+                    <div>
+                        {user?.name}
+                        <a href="#" onClick={onLogout} className='btn-logout'>Logout</a>
+                    </div>
+                </header>
+                <main>
+                    <Outlet/>
+                </main>
+            </div>
+
+        </div>
     )
 }
